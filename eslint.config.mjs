863a27ca32587d78138a8339586b768966c7fc9c
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-01-21 10:23:05 +0800

    ///// RULES \\\\\\
    ///// RULES \\\\\\

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.addRules({
+  compat.rules({
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-empty-object-type": "warn",
     "react/no-unescaped-entities": "off",
     "@next/next/no-img-element": "off", // Disable the no-img-element rule
-    "react/display-name": "off"
-  })
+    "react/display-name": "off",
+  }),
 ];
 
 export default eslintConfig;
